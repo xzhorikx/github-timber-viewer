bf7c4f248222b6c45edba87685413b88a6111eeb
Alex-PC <alexandr.zhurkov@gmail.com>
2023-01-23 21:03:29 +0200

    Adding a parent view to MainActivity

--- app/src/main/java/alex/zhurkov/github_timber_viewer/feature/main/MainActivity.kt
+++ app/src/main/java/alex/zhurkov/github_timber_viewer/feature/main/MainActivity.kt
@@ -1,20 +1,42 @@
 package alex.zhurkov.github_timber_viewer.feature.main
 
+import alex.zhurkov.github_timber_viewer.R
+import alex.zhurkov.github_timber_viewer.common.arch.UIEvent
+import alex.zhurkov.github_timber_viewer.feature.main.di.MainActivityComponent
+import alex.zhurkov.github_timber_viewer.feature.main.presentation.MainActivityEvent
+import alex.zhurkov.github_timber_viewer.feature.main.presentation.MainActivityViewModel
+import alex.zhurkov.github_timber_viewer.feature.main.presentation.MainActivityViewModelFactory
+import alex.zhurkov.github_timber_viewer.feature.main.ui.MainScreen
+import alex.zhurkov.github_timber_viewer.ui.theme.GithubtimberviewerTheme
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import alex.zhurkov.github_timber_viewer.ui.theme.GithubtimberviewerTheme
+import androidx.compose.ui.res.dimensionResource
+import androidx.lifecycle.Observer
+import javax.inject.Inject
 
 class MainActivity : ComponentActivity() {
+
+    private val component: MainActivityComponent by lazy {
+        (application as MainActivityComponent.ComponentProvider).provideMainComponent(this)
+    }
+
+    @Inject
+    lateinit var viewModelFactory: MainActivityViewModelFactory
+    private val viewModel by viewModels<MainActivityViewModel> { viewModelFactory }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        component.inject(this)
+        viewModel.observableEvents.observe(this, Observer(::renderEvent))
         setContent {
             GithubtimberviewerTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,22 +44,34 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainScreen(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(
+                                PaddingValues(
+                                    start = dimensionResource(id = R.dimen.padding_16),
+                                    end = dimensionResource(id = R.dimen.padding_16),
+                                    top = dimensionResource(id = R.dimen.padding_16)
+                                )
+                            ),
+                        uiModel = viewModel.observableModel,
+                    )
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    GithubtimberviewerTheme {
-        Greeting("Android")
+    private fun renderEvent(event: UIEvent) {
+        if (event is MainActivityEvent) {
+            when (event) {
+                is MainActivityEvent.DisplayError -> {
+                    Toast.makeText(
+                        this,
+                        getString(R.string.error_message_template, event.error),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
     }
 }
